a7823b5e17319985f6649beea0d6208fe6315db9
chunlin9578 <654137208@qq.com>
2020-12-08 16:46:39 +0800

    lodash

---
+++ miao/lodash/chunlin9527-lodash.js
@@ -0,0 +1,12 @@
+var chunlin9527 = {
+    compact: function compact(ary) {  // 函数表达式的名字相当于函数内部的变量
+        var result = []
+        compact()
+        for (var i = 0; i < ary.length; i++) {
+            if (a[i]) {
+                result.push(ary[i])
+            }
+        }
+        return result
+    }
+}
